3ecb88ab3ab0366ac5146dd19f0125979bf95c7f
dmlaziuk <dm.laziuk@gmail.com>
2022-05-10 13:16:23 +0400

    bump dependencies

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -7,6 +7,6 @@ import '@testing-library/jest-dom/extend-expect';
 // @ts-ignore
 import { configure } from 'enzyme';
 // @ts-ignore
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
